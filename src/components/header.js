2fc99c89d9bc20d7d1c545dcadd7ff051d5122a9
Chen Li <yichen.li0830@gmail.com>
2017-01-17 17:00:54 -0800

    Adding header

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Header = () => {
+  return (
+      <Text>
+        Albums Header
+      </Text>
+  );
+};
+
+export default Header;
