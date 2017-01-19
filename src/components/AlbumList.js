a6b536fa7c8f70d055837ef2f9c940e1023b6a62
Chen Li <yichen.li0830@gmail.com>
2017-01-19 11:55:18 -0800

    Render albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,21 +2,30 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
 class AlbumList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {albums: []};
+  }
+  
   async componentWillMount() {
     console.log("componentWillMount");
     try {
       const fetchRes = await fetch('https://rallycoding.herokuapp.com/api/music_albums');
-      console.log("fetchRes", fetchRes);
+      // console.log("fetchRes", fetchRes);
       const fetchData = await fetchRes.json();
       // debugger;
-      console.log("fetchData", fetchData);
+      this.setState({albums: fetchData});
+      // console.log("fetchData", fetchData);
     } catch (err) {
       console.error(err);
     }
   }
+  
   render() {
+    console.log(this.state.albums);
+    const albums = this.state.albums.map(album => <Text key = {album.title}> {album.title} </Text>);
     return <View>
-      <Text>AlbumList</Text>
+      {albums}
     </View>
   }
 }
