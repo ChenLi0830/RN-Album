006ac070857c75a6ccd76fc384c674167ef554d0
Chen Li <yichen.li0830@gmail.com>
2017-01-20 10:07:54 -0800

    Style the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,30 @@ import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
 const Button = ()=> {
-  return <View>
-    <TouchableOpacity >
-      <Text>Button</Text>
+  return <TouchableOpacity style = {styles.button}>
+      <Text style={styles.text}>
+        Buy Now
+      </Text>
     </TouchableOpacity>
-  </View>
+};
+
+const styles = {
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginHorizontal: 5,
+  },
+  text: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingVertical: 10,
+  }
 };
 
 export default Button;
