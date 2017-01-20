c1354c64e30c0c7e699bcfc2cd34bc67e601d6d1
Chen Li <yichen.li0830@gmail.com>
2017-01-20 09:52:12 -0800

    Add 'touchableOpacity' for the button

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+
+const Button = ()=> {
+  return <View>
+    <TouchableOpacity >
+      <Text>Button</Text>
+    </TouchableOpacity>
+  </View>
+};
+
+export default Button;
