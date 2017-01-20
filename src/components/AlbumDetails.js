7e5a94f475cf1c11d0a4a010dfb9afb3d1360fed
Chen Li <yichen.li0830@gmail.com>
2017-01-19 16:34:44 -0800

    Adding image and more styles into the album

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,18 +1,58 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetails = (props) => {
+const AlbumDetails = ({album}) => {
+  const {title, artist, thumbnail_image, image} = album;
+  
   return <Card>
     <CardSection>
-      <Text>{props.album.title}</Text>
+      <View style = {styles.thumbnailContainer}>
+        <Image style={styles.thumbnail}
+               source={{uri: thumbnail_image}}
+        />
+      </View>
+      <View style={styles.textView}>
+        <Text style = {styles.headerText}>{title}</Text>
+        <Text>{artist}</Text>
+      </View>
     </CardSection>
   
     <CardSection>
-      <Text>{props.album.title}</Text>
+      <Image style = {styles.imageStyle}
+             source={{uri: image}}
+             />
     </CardSection>
+  
   </Card>
+};
+
+const styles = {
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
+  },
+  
+  thumbnail: {
+    height: 50,
+    width: 50,
+  },
+  
+  headerText: {
+    fontSize: 18,
+  },
+  
+  thumbnailContainer:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  textView: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  }
 };
 
 export default AlbumDetails;
